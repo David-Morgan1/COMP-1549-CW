3d762c6881ca3c3d3041bb0e4f89d018d05dea89
HadilBati <60471083+HadilBati@users.noreply.github.com>
2021-02-12 18:35:21 +0000

    Update Client.java
    Updated Client class

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,5 @@
+package coursework;
+
 import java.io.*;
 import java.net.Socket;
 
@@ -5,20 +7,23 @@ public class Client {
     public static void main(String[] args) {
         try {
             //code to connect the client's socket with the server socket
-            System.out.println("Client Started");
-            Socket soc = new Socket("localhost", 7000);
+            //comment for this to be pushed
+            System.out.println("Client has joined");
+            Socket clientSocket = new Socket("localhost", 7000);
 
             //write and send a message to the server
             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-                // system.in is used to read data from the keyboard
-            System.out.println("Enter a string: ");
+
+            // system.in is used to read data from the keyboard
+            System.out.println("Start the chat: ");
             String str = userInput.readLine();
-            PrintWriter out  = new PrintWriter(soc.getOutputStream(), true);
+            PrintWriter out  = new PrintWriter(clientSocket.getOutputStream(), true);
             out.println(str);
 
             // read the message the server has sent and display on the screen
-            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-                // will read the message from the socket's input stream
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            // will read the message from the socket's input stream
             System.out.println(in.readLine());
         }
         catch (Exception e){
