a14e20fc85e49e0f9ef68d8eb8458e99e489c9f2
Anum <anumsheikh06@gmail>
2021-02-17 21:26:44 +0200

    Addes a new version of the ArrayList in the Client Class. The list needs to be moved to the server so that it doesnt keep resetting.

--- src/Client.java
+++ src/Client.java
@@ -6,25 +6,24 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         try {
-            // Making the ArrayList
-            ArrayList<String> IDArrayList = new ArrayList<String>();
             //code to connect the client's socket with the server socket
             System.out.println("Client Started");
             Socket soc = new Socket("localhost", 7000);
 
+            ArrayList<String> IDArrayList = new ArrayList<String>();
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter UserID:");
+            String UserID = input.next();
+            while (IDArrayList.contains(UserID)) {
+                System.out.print("ID has already been used. Enter another:");
+                UserID = input.next();
+            }
+            IDArrayList.add(UserID);
+            System.out.println(IDArrayList);
+
             //write and send a message to the server
             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
             // system.in is used to read data from the keyboard
-            Scanner input = new Scanner(System.in);
-            System.out.print("Enter your ID:");
-            // This reads what was inputted by the user in the line above
-            String UserID = input.next();
-            // Adds the ID to the ArrayList
-            IDArrayList.add(UserID);
-            // output check that the UserID has been appended to list.
-            System.out.println("UserID Array list is:" + IDArrayList);
-            // checking to see User input has been acknowledged
-            System.out.println("Your ID is:" + UserID);
             System.out.println("Enter a string: ");
             String str = userInput.readLine();
             PrintWriter out  = new PrintWriter(soc.getOutputStream(), true);
