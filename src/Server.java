87651808060eac8a79e1a58fe3965f49d6dff086
Yuhai01 <yuhai.namugunga@gmail.com>
2021-02-24 17:08:51 +0100

    an array list is added to the server (stell need to find out how to read the client's input when the connection establishment is not complete)

--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,9 @@
-package coursework;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
+import java.util.ArrayList;
 
 
 public class Server {
@@ -13,10 +13,29 @@ public class Server {
             // code to connect the server socket with the client socket
             ServerSocket serverSocket = new ServerSocket(7000);
 
-            while(true) {
-                System.out.print("Waiting for client...");
+            //create an array list
+            ArrayList IDArrayList = new ArrayList();
 
+            while(true) {
+                System.out.println("Waiting for client...");
                 Socket clientSocket = serverSocket.accept();
+
+                //send the client a message to input their id
+                //and read the id, then store it in the array list
+                PrintWriter IDout = new PrintWriter(new BufferedOutputStream(clientSocket.getOutputStream()), true);
+                IDout.println("Enter your ID: ");
+
+                BufferedReader ID = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                ID.readLine();
+
+                System.out.println(ID);
+                IDArrayList.add(ID);
+                System.out.println(IDArrayList);
+
+                // code to check if the ID is in the array list
+                // if it is then the client has to enter another name
+                //...
+                // to be continued ;)
 
                 //Print a line Statement which infers on which client just connected
                 System.out.println("Connection Established from " + clientSocket);
@@ -26,7 +45,6 @@ public class Server {
                 //a new thread is made.
                 //Within the new thread, the "HandleClientSocket" method
                 //is there to call the connection of the "clientSocket"
-                //hhihihi
 
 
                 Thread thread = new Thread() {
@@ -65,9 +83,7 @@ public class Server {
     private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
         OutputStream outputStream = clientSocket.getOutputStream();
         for(int i=0; i< 10; i++) {
-            outputStream.write(("Time now is " + new Date() + "\n").getBytes());
             Thread.sleep(1000);
         }
-        clientSocket.close();
     }
 }
