47a4c048e1ebd9f3e0cae7d84eb82cf14c777711
Yuhai01 <yuhai.namugunga@gmail.com>
2021-02-25 14:51:42 +0100

    the new Client, it still needs a lot of fixing though

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,10 @@
-package coursework;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.Buffer;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
@@ -10,20 +13,32 @@ public class Client {
             System.out.println("Client has joined");
             Socket clientSocket = new Socket("localhost", 7000);
 
-            //write and send a message to the server
+            //read message from the server
             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
 
             // system.in is used to read data from the keyboard
-            System.out.println("Start the chat: ");
-            String str = userInput.readLine();
-            PrintWriter out  = new PrintWriter(clientSocket.getOutputStream(), true);
-            out.println(str);
+            //System.out.println("Start the chat: ");
+            //String str = userInput.readLine();
+            //PrintWriter out  = new PrintWriter(clientSocket.getOutputStream(), true);
+            //out.println(str);
 
             // read the message the server has sent and display on the screen
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
             // will read the message from the socket's input stream
             System.out.println(in.readLine());
+
+            // the client is expected to input its id
+            // it then gets sent to the server that reads it
+            // and stores it in the array list
+            Scanner input = new Scanner(System.in);
+            String ID = input.next();
+
+            PrintWriter IDout = new PrintWriter(clientSocket.getOutputStream(), true);
+            IDout.println(ID);
+
+            System.out.println(ID);
+
         }
         catch (Exception e){
             e.printStackTrace();
