641df0779eb1ec31c447287ba43186c2f200139d
david-m01 <dm1532x@greenwich.ac.uk>
2021-03-04 17:22:01 +0000

    Created the base for the server window

--- src/GUI_Server.java
+++ src/GUI_Server.java
@@ -14,7 +14,7 @@ public class GUI_Server extends JFrame {
 	private JPanel contentPane;
 
 	/**
-	 * Launch the application.
+	 * Launch the application
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
