0b6d7fb9dee31309bd4cc149b51afc27971f6ab6
HadilBati <60471083+HadilBati@users.noreply.github.com>
2021-03-13 16:19:39 +0000

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,27 +1,90 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.*;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
 
 public class Server {
+    private static String StringUtils;
+
     public static void main(String[] args) {
+        //TODO auto generated method sub
         try {
             // code to connect the server socket with the client socket
-            System.out.print("Waiting for client...");
-            ServerSocket ss = new ServerSocket(7000);
-            Socket soc = ss.accept();
-            System.out.println("Connection Established");
+            ServerSocket serverSocket = new ServerSocket(7000);
 
-            //read the string which the client has sent
-            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-                // soc.getInputStream() reads data from the sockets input stream
-            String str = in.readLine();
+            //create an array list
+            ArrayList IDArrayList = new ArrayList();
 
-            // send the same message back to the client
-            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
-            out.println("Server says: " + str);
+            while (true) {
+                //This will be displayed on the Server until the connection is established
+                System.out.print("Waiting for client...");
+                Socket clientSocket = serverSocket.accept();
+
+
+                //send the client a message to input their id
+                //and read the id, then store it in the array list
+                PrintWriter IDout = new PrintWriter(new BufferedOutputStream(clientSocket.getOutputStream()), true);
+                IDout.println("Enter your ID: ");
+
+                BufferedReader ID = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                clientSocket.getInputStream();
+                String readID = ID.readLine();
+
+
+                // code to check if the ID is in the array list
+                // if it is then the client has to enter another name
+                while(IDArrayList.contains(readID)) {
+                    IDout.println("ID has already been used. enter another one: ");
+                    clientSocket.getInputStream();
+                    readID = ID.readLine();
+                }
+
+                System.out.println(readID);
+                IDArrayList.add(readID);
+                System.out.println(IDArrayList);
+
+                // Asking User for Port Number input
+                PrintWriter PortOut = new PrintWriter(new BufferedOutputStream(clientSocket.getOutputStream()), true);
+                PortOut.println("Enter Port Number:");
+                BufferedReader Port = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                clientSocket.getInputStream();
+                // PORT NUMBER IS STORED IN 'readPort' variable
+                String readPort = Port.readLine();
+
+                while(!readPort.equals("7000")){
+                    PortOut.println("Wrong Port Number inputted. Try Again: ");
+                    clientSocket.getInputStream();
+                    readPort = Port.readLine();
+                }
+                System.out.println(readPort);
+
+                // Asking User for IP address input
+                PrintWriter IPOut = new PrintWriter(new BufferedOutputStream(clientSocket.getOutputStream()), true);
+                IPOut.println("Enter IP address:");
+                BufferedReader IPaddress = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                clientSocket.getInputStream();
+                //IP address IS STORED IN 'readIP' variable
+                String readIP = IPaddress.readLine();
+                System.out.println(readIP);
+
+
+                //Print a line Statement which infers on which client just connected.
+                //This is displayed on the Server
+                System.out.println("Connection Established from " + IDArrayList);
+
+
+                ServerThreads threads = new ServerThreads(clientSocket);
+                threads.start();
+
+            }
         }
         catch(Exception e){
             e.printStackTrace();
         }
     }
+
+
 }
