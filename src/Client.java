0224c324fefd158787736ba550f5473dfe5e86e2
HadilBati <60471083+HadilBati@users.noreply.github.com>
2021-03-27 15:19:29 +0000

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,27 +1,111 @@
 import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.Buffer;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static java.lang.System.in;
 
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
             //code to connect the client's socket with the server socket
             System.out.println("Client Started");
-            Socket soc = new Socket("localhost", 7000);
+            Socket clientSocket = new Socket("localhost", 7000);
 
-            //write and send a message to the server
-            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-                // system.in is used to read data from the keyboard
-            System.out.println("Enter a string: ");
-            String str = userInput.readLine();
-            PrintWriter out  = new PrintWriter(soc.getOutputStream(), true);
-            out.println(str);
+            //read message from the server
+            BufferedReader input = new BufferedReader(new InputStreamReader(in));
+
+
+            // system.in is used to read data from the keyboard
+            //System.out.println("Start the chat: ");
+            //String str = userInput.readLine();
+            //PrintWriter out  = new PrintWriter(clientSocket.getOutputStream(), true);
+            //out.println(str);
 
             // read the message the server has sent and display on the screen
-            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-                // will read the message from the socket's input stream
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            //returning the output to the server : true statement is to flush the buffer otherwise
+            //we have to do it manually
+            PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            // will read the message from the socket's input stream
             System.out.println(in.readLine());
-        }
-        catch (Exception e){
+
+            //taking the user input
+            Scanner scanner = new Scanner(System.in);
+            String userInput = null;
+            String response;
+            String ID = "empty";
+            String IP = "empty";
+            String Port = "empty";
+
+
+            ClientThreads clientThreads = new ClientThreads(clientSocket);
+
+            new Thread(clientThreads).start();
+            //loop closes when user enters exit command
+
+            do {
+                // the client is expected to input its id
+                // it then gets sent to the server that reads it
+                // and stores it in the array list
+                if (ID.equals("empty")) {
+                    System.out.println("Enter your name ");
+                    userInput = scanner.nextLine();
+                    ID = userInput;
+                    output.println(userInput);
+                }
+
+
+                if (Port.equals("empty")) {
+                    System.out.println("Enter your Port ");
+                    userInput = scanner.nextLine();
+                    Port = userInput;
+                    output.print(userInput);
+                }
+
+                if (IP.equals("empty")) {
+                    System.out.println("Enter your IP");
+                    userInput = scanner.nextLine();
+                    IP = userInput;
+                    output.print(userInput);
+                }
+
+                if (userInput.equals("exit")) {
+                    break;
+                } else {
+                    String message = ("(" + ID + ")" + " message : ");
+                    System.out.println(message);
+                    userInput = scanner.nextLine();
+                    output.println(message + " " + in);
+                    if (in.equals("exit")) {
+                        //reading the input from server
+                        break;
+
+                    }
+                }
+            } while (!userInput.equals("exit"));
+
+
+            //System.out.println("Connection Established from " + clientSocket);
+
+            ConcurrentHashMap<String, Socket> activeClients = new ConcurrentHashMap<String, Socket>();
+
+            activeClients.put(clientSocket.getInetAddress().getHostAddress(), clientSocket);
+
+            for (String clientHost : activeClients.keySet()) {
+
+
+
+
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
